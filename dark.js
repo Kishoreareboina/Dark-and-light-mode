29f17cdbc6e0dd6ad13dff8e47ce348b5fae9f8c
Kishoreareboina <167310339+Kishoreareboina@users.noreply.github.com>
2024-05-23 17:07:05 +0530

    dark.js

--- dark.js
+++ dark.js
@@ -5,18 +5,18 @@ let headingEl = document.getElementById("heading");
 let lightThemeImgUrl =
   "url('https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/change-theme-light-bg.png')";
 
-let darkThemeImgUrl =
+let dayThemeImgUrl =
   "url('https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/change-theme-dark-bg.png')";
 
 function changeTheme(event) {
   if (event.key === "Enter") {
     let themeUserInputVal = themeUserInputEl.value;
 
-    if (themeUserInputVal === "light") {
-      bgContainerEl.style.backgroundImage = darkThemeImgUrl;
+    if (themeUserInputVal === "night") {
+      bgContainerEl.style.backgroundImage = dayThemeImgUrl;
       headingEl.style.color = "#4d0143";
-    } else if (themeUserInputVal === "dark") {
-      bgContainerEl.style.backgroundImage = lightThemeImgUrl;
+    } else if (themeUserInputVal === "day") {
+      bgContainerEl.style.backgroundImage = nightThemeImgUrl;
       headingEl.style.color = "#ffffff";
     } else {
       alert("enter the valid theme");
